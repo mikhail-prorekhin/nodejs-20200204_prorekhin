8a918970b8a608d7d7d1cd27e798bf2d6bc34622
Mikhail Prorekhin <mikhail.prorekhin@target.com.au>
2020-02-26 16:07:59 +1100

    add solution for 2-module 1-task

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -1,13 +1,21 @@
-const stream = require('stream');
-const LimitExceededError = require('./LimitExceededError');
+const stream = require("stream");
+const LimitExceededError = require("./LimitExceededError");
 
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    const { limit } = options;
+    this.limit = limit;
+    this.count = 0;
   }
 
   _transform(chunk, encoding, callback) {
-
+    const size = chunk.toString().length;
+    if (this.count + size > this.limit) {
+      this.emit("error", new LimitExceededError());
+    }
+    this.count += size;
+    callback(null, chunk);
   }
 }
 
