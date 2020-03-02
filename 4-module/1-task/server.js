603407c066a966110f63c84849572b5b163833dc
Mikhail Prorekhin <mikhail.prorekhin@target.com.au>
2020-03-03 08:46:01 +1100

    add solution for 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,22 +1,45 @@
-const url = require('url');
-const http = require('http');
-const path = require('path');
+const url = require("url");
+const http = require("http");
+const path = require("path");
+const fileSystem = require("fs");
 
 const server = new http.Server();
 
-server.on('request', (req, res) => {
+const isNestedFile = path => path.split("/").length > 1;
+
+server.on("request", (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+  const filepath = path.join(__dirname, "files", pathname);
 
   switch (req.method) {
-    case 'GET':
+    case "GET":
+      if (isNestedFile(pathname)) {
+        res.statusCode = 400;
+        res.end(`the path ${pathname} is nested`);
+        break;
+      }
+      if (!fileSystem.existsSync(filepath)) {
+        res.statusCode = 404;
+        res.end(`file ${pathname} does not exists`);
+        break;
+      }
+
+      const stat = fileSystem.statSync(filepath);
+      //console.log(JSON.stringify(stat));
+      res.writeHead(200, {
+        "Content-Length": stat.size
+      });
+
+      const readStream = fileSystem.createReadStream(filepath);
+
+      readStream.pipe(res);
 
       break;
 
     default:
       res.statusCode = 501;
-      res.end('Not implemented');
+      res.end("Not implemented");
   }
 });
 
