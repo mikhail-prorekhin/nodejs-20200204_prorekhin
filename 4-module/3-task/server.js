8f8082b936a9aadd2de23f12b7aa52b037aa37f8
Mikhail Prorekhin <mikhail.prorekhin@target.com.au>
2020-03-03 12:59:41 +1100

    add solution for 4-module 3-task

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,22 +1,36 @@
-const url = require('url');
-const http = require('http');
-const path = require('path');
+const url = require("url");
+const http = require("http");
+const path = require("path");
+const fileSystem = require("fs");
 
+const isNestedFile = path => path.split("/").length > 1;
 const server = new http.Server();
 
-server.on('request', (req, res) => {
+server.on("request", (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+  const filepath = path.join(__dirname, "files", pathname);
 
   switch (req.method) {
-    case 'DELETE':
-
+    case "DELETE":
+      if (isNestedFile(pathname)) {
+        res.statusCode = 400;
+        res.end(`the path ${pathname} is nested`);
+        break;
+      }
+      if (!fileSystem.existsSync(filepath)) {
+        res.statusCode = 404;
+        res.end(`the file ${pathname} does not exists`);
+        break;
+      }
+      fileSystem.unlinkSync(filepath);
+      res.statusCode = 200;
+      res.end("0k");
       break;
 
     default:
       res.statusCode = 501;
-      res.end('Not implemented');
+      res.end("Not implemented");
   }
 });
 
