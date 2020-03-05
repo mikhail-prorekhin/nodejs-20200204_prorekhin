8cb5f4b51bc5be6fbf42b31c245426acdc1559e4
Mikhail Prorekhin <mikhail.prorekhin@target.com.au>
2020-03-06 10:34:03 +1100

    add solution for 6-module 2-task

--- 6-module/2-task/controllers/categories.js
+++ 6-module/2-task/controllers/categories.js
@@ -1,3 +1,47 @@
+const connection = require("../libs/connection");
+const Category = require("../models/Category");
+const mongoose = require("mongoose");
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  // let category1 = await Category.create({
+  //   title: "Category1",
+  //   subcategories: [
+  //     {
+  //       title: "Subcategory1"
+  //     }
+  //   ]
+  // });
+  // let category2 = await Category.create({
+  //   title: "Category2",
+  //   subcategories: [
+  //     {
+  //       title: "Subcategory2"
+  //     }
+  //   ]
+  // });
+  // let category3 = await Category.create({
+  //   title: "Category3",
+  //   subcategories: [
+  //     {
+  //       title: "Subcategory3"
+  //     }
+  //   ]
+  // });
+  // await category1.save();
+  // await category2.save();
+  // await category3.save();
+  const _categories = await Category.find();
+
+  const categories = _categories.map(_rec => ({
+    id: _rec._id,
+    title: _rec.title,
+    subcategories: _rec.subcategories.map(_sub => ({
+      id: _sub._id,
+      title: _sub.title
+    }))
+  }));
+
+  ctx.body = { categories };
+  ctx.status = 200;
+  await next();
 };
--- 6-module/2-task/controllers/products.js
+++ 6-module/2-task/controllers/products.js
@@ -1,12 +1,92 @@
-module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {};
+const mongoose = require("mongoose");
+const connection = require("../libs/connection");
+const Category = require("../models/Category");
+const Product = require("../models/Product");
+const { ObjectId } = require("mongodb");
+
+const transformProduct = ({
+  title,
+  description,
+  price,
+  images,
+  category,
+  subcategory,
+  _id
+}) => ({
+  title,
+  description,
+  price,
+  images,
+  category,
+  subcategory,
+  id: _id
+});
+
+module.exports.productsBySubcategory = async function productsBySubcategory(
+  ctx,
+  next
+) {
+  const { subcategory } = ctx.request.query;
+  if (!subcategory) {
+    await next();
+    return;
+  }
+
+  if (!ObjectId.isValid(subcategory)) {
+    ctx.throw(400, "invalid id");
+  }
+  const _products = await Product.find({ subcategory });
+  const products = _products.map(transformProduct);
+  ctx.status = 200;
+  ctx.body = {
+    products
+  };
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  // let product;
+
+  // product = await Product.create({
+  //   title: "Product1",
+  //   description: "Description1",
+  //   price: 10,
+  //   category: ObjectId("5e60af67a7a90e7b42dd0764"),
+  //   subcategory: ObjectId("5e60af67a7a90e7b42dd0765"),
+  //   images: ["image1"]
+  // });
+
+  // product = await Product.create({
+  //   title: "Product2",
+  //   description: "Description2",
+  //   price: 33,
+  //   category: ObjectId("5e60af67a7a90e7b42dd0766"),
+  //   subcategory: ObjectId("5e60af67a7a90e7b42dd0767"),
+  //   images: ["image12", "image13", "image14"]
+  // });
+
+  const _products = await Product.find();
+  //await mongoose.disconnect();
+  await next();
+  const products = _products.map(transformProduct);
+  ctx.status = 200;
+  ctx.body = {
+    products
+  };
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
-};
+  const { id } = ctx.params;
 
+  if (!ObjectId.isValid(id)) {
+    ctx.throw(400, "invalid id");
+  }
+
+  const _product = await Product.findById(id);
+
+  await next();
+  if (!_product) {
+    ctx.throw(404, "Not found");
+  }
+  ctx.body = { product: transformProduct(_product) };
+  ctx.status = 200;
+};
