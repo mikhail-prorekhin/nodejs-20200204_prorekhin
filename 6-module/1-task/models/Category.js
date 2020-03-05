c3150789cb2ec6d14f8d1ca3a1aab839ee088917
Mikhail Prorekhin <mikhail.prorekhin@target.com.au>
2020-03-05 15:55:08 +1100

    add solution for 6-module 1-task

--- 6-module/1-task/models/Category.js
+++ 6-module/1-task/models/Category.js
@@ -1,11 +1,19 @@
-const mongoose = require('mongoose');
-const connection = require('../libs/connection');
+const mongoose = require("mongoose");
+const connection = require("../libs/connection");
 
 const subCategorySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  }
 });
 
 const categorySchema = new mongoose.Schema({
   subcategories: [subCategorySchema],
+  title: {
+    type: String,
+    required: true
+  }
 });
 
-module.exports = connection.model('Category', categorySchema);
+module.exports = connection.model("Category", categorySchema);
--- 6-module/1-task/models/Product.js
+++ 6-module/1-task/models/Product.js
@@ -1,7 +1,48 @@
-const mongoose = require('mongoose');
-const connection = require('../libs/connection');
+const mongoose = require("mongoose");
+const connection = require("../libs/connection");
 
 const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true
+  },
+  subcategory: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
+  images: [String]
 });
 
-module.exports = connection.model('Product', productSchema);
+module.exports = connection.model("Product", productSchema);
+/**
+- `title`, в этом поле будет находиться название товара, например, "Коляска Adamex Barletta".
+    - строковое
+    - обязательное
+- `description`, описание товара.
+    - строковое
+    - обязательное
+- `price`, цена товара.
+    - числовое
+    - обязательное
+- `category`, идентификатор категории товара.
+    - ObjectId (ref='Category')
+    - обязательное
+- `subcategory`, идентификатор категории товара.
+    - ObjectId
+    - обязательное
+- `images`, массив ссылок изображений
+    - массив строк
+ */
