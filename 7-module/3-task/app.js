84e1de045a7f2bab4bd9841f0e288e75ff3b0ccd
Mikhail Prorekhin <mikhail.prorekhin@target.com.au>
2020-03-11 19:00:18 +1100

    add solution for 7-module 3-task

--- 7-module/3-task/app.js
+++ 7-module/3-task/app.js
@@ -1,18 +1,19 @@
-const path = require('path');
-const Koa = require('koa');
-const Router = require('koa-router');
-const Session = require('./models/Session');
-const uuid = require('uuid/v4');
-const handleMongooseValidationError = require('./libs/validationErrors');
-const mustBeAuthenticated = require('./libs/mustBeAuthenticated');
-const {login} = require('./controllers/login');
-const {oauth, oauthCallback} = require('./controllers/oauth');
-const {me} = require('./controllers/me');
+const path = require("path");
+const Koa = require("koa");
+const Router = require("koa-router");
+const Session = require("./models/Session");
+const uuid = require("uuid/v4");
+const handleMongooseValidationError = require("./libs/validationErrors");
+const mustBeAuthenticated = require("./libs/mustBeAuthenticated");
+const { login } = require("./controllers/login");
+const { oauth, oauthCallback } = require("./controllers/oauth");
+const { me } = require("./controllers/me");
+const mongoose = require("mongoose");
 
 const app = new Koa();
 
-app.use(require('koa-static')(path.join(__dirname, 'public')));
-app.use(require('koa-bodyparser')());
+app.use(require("koa-static")(path.join(__dirname, "public")));
+app.use(require("koa-bodyparser")());
 
 app.use(async (ctx, next) => {
   try {
@@ -20,51 +21,73 @@ app.use(async (ctx, next) => {
   } catch (err) {
     if (err.status) {
       ctx.status = err.status;
-      ctx.body = {error: err.message};
+      ctx.body = { error: err.message };
     } else {
       console.error(err);
       ctx.status = 500;
-      ctx.body = {error: 'Internal server error'};
+      ctx.body = { error: "Internal server error" };
     }
   }
 });
 
 app.use((ctx, next) => {
   ctx.login = async function(user) {
-    const token = uuid();
+    const id = user._id;
+    const session = await Session.findOne({
+      user: mongoose.Types.ObjectId(id)
+    });
 
-    return token;
+    return session.token;
   };
 
   return next();
 });
 
-const router = new Router({prefix: '/api'});
+const router = new Router({ prefix: "/api" });
 
 router.use(async (ctx, next) => {
-  const header = ctx.request.get('Authorization');
+  const header = ctx.request.get("Authorization");
   if (!header) return next();
+
+  const parts = header.split(" ");
+  if (!parts.includes("Bearer")) {
+    return next();
+  }
+  const index = parts.indexOf("Bearer");
+  const token = parts[index + 1];
+
+  const session = await Session.findOne({ token }).populate("user");
+
+  if (!session) {
+    ctx.status = 401;
+    ctx.body = { error: "Неверный аутентификационный токен" };
+    return;
+  } else {
+    session.lastVisit = new Date();
+    session.save();
+    ctx.user = session.user;
+  }
 
   return next();
 });
 
-router.post('/login', login);
+router.post("/login", login);
 
-router.get('/oauth/:provider', oauth);
-router.post('/oauth_callback', handleMongooseValidationError, oauthCallback);
+router.get("/oauth/:provider", oauth);
+router.post("/oauth_callback", handleMongooseValidationError, oauthCallback);
 
-router.get('/me', me);
+router.get("/me", me);
 
 app.use(router.routes());
 
 // this for HTML5 history in browser
-const fs = require('fs');
+const fs = require("fs");
 
-const index = fs.readFileSync(path.join(__dirname, 'public/index.html'));
-app.use(async (ctx) => {
-  if (ctx.url.startsWith('/api') || ctx.method !== 'GET') return;
-  
-  ctx.set('content-type', 'text/html');
+const index = fs.readFileSync(path.join(__dirname, "public/index.html"));
+app.use(async ctx => {
+  if (ctx.url.startsWith("/api") || ctx.method !== "GET") return;
+
+  ctx.set("content-type", "text/html");
   ctx.body = index;
 });
 
--- 7-module/3-task/controllers/me.js
+++ 7-module/3-task/controllers/me.js
@@ -1,6 +1,12 @@
 module.exports.me = async function me(ctx, next) {
-  ctx.body = {
-    email: ctx.user.email,
-    displayName: ctx.user.displayName,
-  };
+  if (ctx.user) {
+    ctx.body = {
+      email: ctx.user.email,
+      displayName: ctx.user.displayName
+    };
+  } else {
+    ctx.status = 401;
+
+    ctx.body = { error: "Пользователь не залогинен" };
+  }
 };
