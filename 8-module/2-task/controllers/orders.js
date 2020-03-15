77c3862e151f36e1fde4f67a1bd2ecb0687e2d2f
Mikhail Prorekhin <mikhail.prorekhin@gmail.com>
2020-03-16 00:26:26 +1100

    add solution for 8-module 2-task

--- 8-module/2-task/controllers/orders.js
+++ 8-module/2-task/controllers/orders.js
@@ -1,8 +1,56 @@
-const Order = require('../models/Order');
-const sendMail = require('../libs/sendMail');
+const Order = require("../models/Order");
+const sendMail = require("../libs/sendMail");
+const Product = require("./../models/Product");
 
 module.exports.checkout = async function checkout(ctx, next) {
+  const { phone, address } = ctx.request.body;
+  const productId = ctx.request.body.product;
+  const { user } = ctx;
+
+  if (!user) {
+    ctx.throw(401, "пользователь не авторизован");
+  }
+  //   if (!productId || !phone || !address) {
+  //     ctx.throw(
+  //       400,
+  //       "описанием ошибки валидации при неправильно сформированном запросе"
+  //     );
+  //   }
+
+  const product = await Product.findById(productId);
+
+  try {
+    let order = await Order.create({
+      user,
+      product,
+      phone,
+      address
+    });
+    await order.validate();
+
+    await order.save();
+    ctx.response.body = { order: order._id };
+    await sendMail({
+      template: "order-confirmation",
+      locals: { id: order._id, product },
+      to: user.email,
+      subject: "Подтвердите почту"
+    });
+  } catch (e) {
+    ctx.status = 400;
+    ctx.body = { errors: {} };
+    for (let path in e.errors) {
+      ctx.body.errors[path] = e.errors[path].message;
+    }
+  }
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
+  const { user } = ctx;
+
+  if (!user) {
+    ctx.throw(401, "пользователь не авторизован");
+  }
+  const orders = await Order.find({ user });
+  ctx.response.body = { orders };
 };
--- 8-module/2-task/libs/sendMail.js
+++ 8-module/2-task/libs/sendMail.js
@@ -1,56 +1,63 @@
-const juice = require('juice');
-const config = require('../config');
-const path = require('path');
-const pug = require('pug');
+const juice = require("juice");
+const config = require("../config");
+const path = require("path");
+const pug = require("pug");
 
-const nodemailer = require('nodemailer');
-const htmlToText = require('nodemailer-html-to-text').htmlToText;
-const SMTPTransport = require('nodemailer-smtp-transport');
-const StubTransport = require('nodemailer-stub-transport');
+const nodemailer = require("nodemailer");
+const htmlToText = require("nodemailer-html-to-text").htmlToText;
+const SMTPTransport = require("nodemailer-smtp-transport");
+const StubTransport = require("nodemailer-stub-transport");
 
-const transportEngine = process.env.NODE_ENV === 'test'
-  ? new StubTransport()
-  : new SMTPTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-      user: config.mailer.user,
-      pass: config.mailer.password,
-    },
-  });
+const transportEngine =
+  process.env.NODE_ENV === "test"
+    ? new StubTransport()
+    : new SMTPTransport({
+        // host: 'smtp.gmail.com',
+        // port: 465,
+        // secure: true,
+        // auth: {
+        //   user: config.mailer.user,
+        //   pass: config.mailer.password,
+        // },
+        host: "smtp.ethereal.email",
+        port: 587,
+        auth: {
+          user: "elisa.cremin19@ethereal.email",
+          pass: "eTEsFyh4qtxTHgeqmv"
+        }
+      });
 
 const transport = nodemailer.createTransport(transportEngine);
 
-transport.use('compile', htmlToText());
+transport.use("compile", htmlToText());
 
 /*
-* sendMail - функция, отправляющая письмо на указанный адрес
-* options - объект, содержащий опции для отправки писем:
-* options.template - имя файла, содержащего шаблон письма
-* options.locals - объект с переменными, которые будут переданы в шаблон
-* options.to - email, на который будет отправлено письмо
-* options.subject - тема письма
-* пример:
-*     await sendMail({
-*       template: 'confirmation',
-*       locals: {token: 'token'},
-*       to: 'user@mail.com',
-*       subject: 'Подтвердите почту',
-*     });
-* */
+ * sendMail - функция, отправляющая письмо на указанный адрес
+ * options - объект, содержащий опции для отправки писем:
+ * options.template - имя файла, содержащего шаблон письма
+ * options.locals - объект с переменными, которые будут переданы в шаблон
+ * options.to - email, на который будет отправлено письмо
+ * options.subject - тема письма
+ * пример:
+ *     await sendMail({
+ *       template: 'confirmation',
+ *       locals: {token: 'token'},
+ *       to: 'user@mail.com',
+ *       subject: 'Подтвердите почту',
+ *     });
+ * */
 module.exports = async function sendMail(options) {
   const html = pug.renderFile(
-      path.join(__dirname, '../templates', options.template) + '.pug',
-      options.locals || {},
+    path.join(__dirname, "../templates", options.template) + ".pug",
+    options.locals || {}
   );
 
   const message = {
     html: juice(html),
     to: {
-      address: options.to,
+      address: options.to
     },
-    subject: options.subject,
+    subject: options.subject
   };
 
   return await transport.sendMail(message);
