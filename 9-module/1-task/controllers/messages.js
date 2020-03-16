e12aeebbf64e4c6b7bb8b30449ffdd51479e23dc
Mikhail Prorekhin <mikhail.prorekhin@gmail.com>
2020-03-16 23:44:15 +1100

    add solution for 9-module 1-task

--- 9-module/1-task/controllers/messages.js
+++ 9-module/1-task/controllers/messages.js
@@ -1,9 +1,17 @@
 const Message = require("../models/Message");
 
 module.exports.messageList = async function messages(ctx, next) {
-  const message = await Message.find;
-  const messages = Message.find({})
+  const { user } = ctx;
+
+  const messageList = await Message.find({ chat: user._id })
     .sort("-date")
     .limit(20);
+  const messages = messageList.map(record => ({
+    date: record.date,
+    text: record.text,
+    id: record._id,
+    user: record.user
+  }));
+
   ctx.body = { messages };
 };
